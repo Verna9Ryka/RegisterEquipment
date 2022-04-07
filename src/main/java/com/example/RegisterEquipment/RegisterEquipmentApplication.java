55bc772bb5bc8b08af94034b46513b77471bdcbd
Verna9Ryka <N0Voselcev_DmiTRiy_NSK@mail.ru>
2022-04-07 14:22:02 +0700

    Добавлен каркас проекта

---
+++ src/main/java/com/example/RegisterEquipment/RegisterEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.RegisterEquipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterEquipmentApplication.class, args);
+	}
+
+}
